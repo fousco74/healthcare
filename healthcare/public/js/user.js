db6e8d277fbf9db487ba3b48a84e648cd0b9f13a
KONE Fousseni <konefousseni66@gmail.com>
2026-07-12 14:49:52 +0000

    refactor(user): harmonise module profile name to Koralis Health
    Align the default module profile with the rest of the branding
    (Koralis Santé -> Koralis Health). The Module Profile record and the 4+9
    existing System Users were renamed/backfilled in the site DB as well.

--- healthcare/public/js/user.js
+++ healthcare/public/js/user.js
@@ -2,7 +2,7 @@ frappe.ui.form.on("User", {
 	refresh: function (frm) {
 		frm.set_df_property("module_profile", "read_only", 1);
 		if (!frm.doc.module_profile) {
-			frm.set_value("module_profile", "Koralis Santé");
+			frm.set_value("module_profile", "Koralis Health");
 		}
 	},
 });
